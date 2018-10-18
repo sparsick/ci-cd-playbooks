e89ea3ba604e83949ed549f6b3f7cdbb80429503
sparsick <sparsick@web.de>
2018-10-18 14:13:31 +0200

    jenkins - refactoring

--- roles/jenkins/files/init.groovy.d/0-skip-setup-wizard.groovy
+++ roles/jenkins/files/init.groovy.d/0-skip-setup-wizard.groovy
@@ -4,6 +4,6 @@ import jenkins.model.*
 import hudson.util.*;
 import jenkins.install.*;
 
-def instance = Jenkins.getInstance()
+def instance = Jenkins.instance
 
 instance.setInstallState(InstallState.INITIAL_SETUP_COMPLETED)
